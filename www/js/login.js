383c54cb48892da5fe5a49361914b68d44712646
linsinjia <linsinjia@gmail.com>
2015-03-19 07:55:54 +0800

    最初版,尚未有功能

---
+++ www/js/login.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function(){
+	$("#login_btn").click(function(){
+		window.location.replace('menu.html');
+	});
+	
+	$("#server_btn").click(function(){
+		window.location.replace('server_info.html');
+	});
+	
+	$("#vm_btn").click(function(){
+		window.location.replace('vm_info.html');
+	});
+	
+	$("#log_out").click(function(){
+		window.location.replace('index.html');
+	});
+	//
+	$("#sever_1").click(function(){
+		window.location.replace('server_detail.html');
+	});
+	
+	$("#back_1").click(function(){
+		window.location.replace('menu.html');
+	});
+	
+	$("#vm_1").click(function(){
+		window.location.replace('vm_detail.html');
+	});
+	
+	$("#back_2").click(function(){
+		window.location.replace('menu.html');
+	});
+	
+});		
